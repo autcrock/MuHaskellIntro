083acce8b7bc040b9135754fbb92e75b9448a8b6
Mike Thomas <Mike Thomas>
2021-05-19 08:10:13 +1000

    Add streaming endpoint.

--- src/Main.hs
+++ src/Main.hs
@@ -13,15 +13,36 @@ import Mu.Server
 
 import Schema
 
+import Data.Conduit as C
+import Data.Conduit.List as C
+--import Data.Conduit.Util as C
+
 main :: IO ()
 main = runGRpcApp msgProtoBuf 8080 server
 
 server :: MonadServer m => SingleServerT i Service m _
-server = singleService (method @"SayHello" sayHello)
+server = singleService ( method @"SayHello" sayHello
+                        , method @"SayManyHellos" sayManyHellos )
+
+-- type Service
+--   = 'Service "Greeter"
+--       '[ 'Method "SayHello" sayHello
+--        , 'Method "SayManyHellos" '[]
+--         '[ 'ArgStream 'Nothing '[] ('FromSchema QuickstartSchema "HelloRequest")]
+--         ('RetStream ('FromSchema QuickstartSchema "HelloResponse")) ]
 
 sayHello :: (MonadServer m) => HelloRequestMessage -> m HelloReplyMessage
 sayHello (HelloRequestMessage nm)
   = pure $ HelloReplyMessage ("hi, " <> nm)
+
+sayManyHellos
+  :: (MonadServer m)
+  => ConduitT () HelloRequestMessage m ()
+  -> ConduitT HelloReplyMessage Void m ()
+  -> m ()
+
+sayManyHellos source sink
+  = runConduit $ source .| C.mapM sayHello .| sink
 
 -- Optics
 -- sayHello :: (MonadServer m) => HelloRequestMessage' -> m HelloReplyMessage'
